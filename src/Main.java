c6d82c4302b18057f69921adfff8ead52628f01a
JorgeBatista <jorgebatista.jb24@gmail.com>
2023-06-12 10:47:11 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Word!");
+
+        byte variavelbyte = 120;
+        System.out.println(variavelbyte);
+
+        //short -32768 a 32767
+        //char -0 a 65535
+        //int -2b a 2b
+        //long -9t a 9t
+
+        int variavelint = 2147483;
+
+        System.out.println(variavelint);
+        }
+    }
+
+
